696818e2c5c17f1332966fda0f0b119644b09de7
Sambhav073 <115652970+Sambhav073@users.noreply.github.com>
2024-11-01 23:35:40 +0530

    Add files via upload

---
+++ Arrays_List/src/Main.kt
@@ -0,0 +1,32 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+//    var marks=arrayOf(60,70,80,90,100)
+//    //to print values of array in kotlin we use get function
+//    println(marks.get(3))
+//    //to print or get length of array we size function
+//    println(marks.size)
+//    //we can store multiple types of data in array as well though not recommended
+//    val data=arrayOf("Sambhav",12220163,7.8)
+//    println(data.get(0))
+//    println(data.get(1))
+//    println(data.get(2))
+//    //to print or iterate in array
+//    for(i in marks){
+//        println(i)
+//    }
+
+
+
+//    //List
+//    var marks= mutableListOf(10,20,30,40,50)
+//    marks.add(48)
+//    println(marks)
+//    println(marks.get(5))
+//    marks.remove(10)
+//    println(marks.get(0))
+
+    //Arrays of nulls
+    val nullArray= arrayOfNulls<String>(3)
+    println(nullArray.contentToString())
+}
