d71b7bd5c7810a7207fd6cc1f1107637a913860b
Sambhav073 <115652970+Sambhav073@users.noreply.github.com>
2024-11-01 23:40:37 +0530

    Add files via upload

---
+++ Variables/src/Main.kt
@@ -0,0 +1,60 @@
+import kotlin.math.max
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var userName: String ="Sambhav"
+    userName="Sambhav"
+    var age:Int=20
+    println("Hello $userName! your age is $age")
+    println("length of string is ${userName.length}")
+    println("to uppercase the String ${userName.toUpperCase()}")
+    println("substring of string ${userName.substring(0,3)}")
+
+    //Int dataType
+//    val number:Int=22
+//    val maxIntegerValue:Int=Int.MAX_VALUE
+//    val minIntgerValue:Int=Int.MIN_VALUE
+//    println("Int max value is $maxIntegerValue")
+//    println("Int min value is $minIntgerValue")
+
+    //Byte dataType
+//    val maxByteValue:Byte=Byte.MAX_VALUE
+//    val minByteValue:Byte=Byte.MIN_VALUE
+//    println("Byte max value is $maxByteValue")
+//    println("Byte min value is $minByteValue")
+
+
+//    //Short DataType
+//    val maxShortValue:Short=Short.MAX_VALUE
+//    val minShortValue:Short=Short.MIN_VALUE
+//    println("Short max value is $maxShortValue")
+//    println("Short min value is $minShortValue")
+
+
+    //Long DataType
+//    val maxLongValue:Long=Long.MAX_VALUE
+//    val minLongValue:Long=Long.MIN_VALUE
+//    println("Long max value is $maxLongValue")
+//    println("Long min value is $minLongValue")
+
+
+
+//    //float DataType
+//    val maxFloatValue:Float= Float.MAX_VALUE
+//    val minFloatValue:Float=Float.MIN_VALUE
+//    println("Float max value is: $maxFloatValue")
+//    println("Float min value is: $minFloatValue")
+
+    //Double dataType
+//    val maxDoubleValue:Double= Double.MAX_VALUE
+//    val minDoubleValue:Double=Double.MIN_VALUE
+//    println("Double max value is: $maxDoubleValue")
+//    println("Double min value is: $minDoubleValue")
+
+
+    //char Datatype
+//    val  ch:Char='A'
+//    println(ch)
+    
+}
