29babaaba42fa2e8e7df3bc88ad9a2a1d4ed0572
Sambhav073 <115652970+Sambhav073@users.noreply.github.com>
2024-11-01 23:37:46 +0530

    Add files via upload

---
+++ ControlFlowWhen/src/Main.kt
@@ -0,0 +1,48 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+//    val alarm=12
+//    when(alarm){
+//        12 -> println("The time is $alarm")
+//        7 -> println("The time is $alarm")
+//        14 -> println("The time is $alarm")
+//        else -> println("The time is $alarm")
+//    }
+
+    //, work as or
+//    when(alarm){
+//        12,7,14 -> println("The time is $alarm")
+//        else -> println("The time is $alarm")
+//    }
+
+    //we can check in range as well
+//    when(alarm){
+//        in 1..15 -> println("The number is in range")  //we can also add ! operator to check not in range
+//        else -> println("The time is $alarm")
+//    }
+
+
+    //when can also compare
+//    val text=when{
+//        alarm<=12->"alarm exists"
+//        else->"not exist"
+//    }
+//    println(text)
+//    var numbers= mutableListOf(2,4,-1,5,3)
+//    for(num in numbers){
+//        when {
+//            num<0->{
+//                println("end")
+//                break
+//            }
+//            num%2==0->println("even")
+//            else->println("odd")
+//        }
+//    }
+
+//    var i=0
+//    do{
+//        println(0)
+//        i=i*2
+//    }while(i<16)
+}
