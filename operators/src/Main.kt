7f4fef2b8396e88a336590551ff9d4b5124b31b7
Sambhav073 <115652970+Sambhav073@users.noreply.github.com>
2024-11-01 23:40:12 +0530

    Add files via upload

---
+++ operators/src/Main.kt
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+//    val x =5
+//    val y=3.0
+//    //val result=x+y
+//    //println("result = $result")
+//    println("x + y = ${x+y}")
+//    println("x - y = ${x-y}")
+//    println("x * y = ${x*y}")
+//    println("x / y = ${x/y}")
+//    println("x % y = ${x%y}")
+//
+//    //operator precedence
+//    println("3 + 3 * 4 = ${3+3*4}")
+
+
+    //if else
+    //val isActive = false
+//    if (isActive) {
+//        println("The user is active")
+//    } else {
+//        println("The user is inactive")
+//    }
+}
